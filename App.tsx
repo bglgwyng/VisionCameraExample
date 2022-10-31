3b6cbdcd0962b5326b0b21bdbdd64a24c137e4c1
bglgwyng <bglgwyng@gmail.com>
2022-10-31 17:08:46 +0900

    Working without use_frameworks!.

--- App.tsx
+++ App.tsx
@@ -7,114 +7,73 @@
  *
  * @format
  */
+import 'react-native-reanimated';
 
-import React, {type PropsWithChildren} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  Camera,
+  CameraPermissionStatus,
+  Frame,
+  useCameraDevices,
+  useFrameProcessor,
+} from 'react-native-vision-camera';
+import {BarcodeFormat, scanBarcodes} from 'vision-camera-code-scanner';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const CameraApp = () => {
+  const devices = useCameraDevices('wide-angle-camera');
+  const device = devices.front;
+  const {width, height} = useWindowDimensions();
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  const frameProcessor = useFrameProcessor(frame => {
+    'worklet';
+
+    // If you comment out the following line, the app won't crash
+    // xyz(frame);
+
+    // `scanBarcodes` works fine
+    console.info(scanBarcodes(frame, [BarcodeFormat.QR_CODE]));
+  }, []);
+
+  return device ? (
+    <Camera
+      device={device}
+      style={{width, height}}
+      frameProcessor={frameProcessor}
+      frameProcessorFps={5}
+      isActive
+    />
+  ) : null;
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [cameraPermission, setCameraPermission] =
+    useState<CameraPermissionStatus>();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    if (cameraPermission === undefined) {
+      Camera.getCameraPermissionStatus().then(setCameraPermission);
+    } else {
+      Camera.requestCameraPermission().then(setCameraPermission);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [!!cameraPermission]);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  if (cameraPermission == null) {
+    return null;
+  }
+
+  const showPermissionsPage = cameraPermission !== 'authorized';
+
+  return showPermissionsPage ? (
+    <View>
+      <TouchableOpacity>
+        <Text />
+      </TouchableOpacity>
+    </View>
+  ) : (
+    <CameraApp />
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
